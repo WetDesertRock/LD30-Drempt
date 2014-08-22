5b7c77e0f3932ef4c75e7654a28aa08f5a0d1e5b
WetDesertRock <WetDesertRock@users.noreply.github.com>
2014-08-22 14:21:45 -0700

    Getting ready for LD, hello LD

---
+++ main.lua
@@ -0,0 +1,23 @@
+function love.load()
+    love.graphics.setBackgroundColor(190, 187, 177)
+end
+
+function love.keypressed(key,isrepeat)
+end
+
+function love.keypressed(k)
+end
+
+
+function love.keyreleased(k)
+end
+
+
+function love.update(dt)
+end
+
+
+function love.draw()
+    love.graphics.setColor(0,0,0)
+    love.graphics.printf("Welcome LD30",0,(love.graphics.getHeight()/2)-5,love.graphics.getWidth(),"center")
+end
